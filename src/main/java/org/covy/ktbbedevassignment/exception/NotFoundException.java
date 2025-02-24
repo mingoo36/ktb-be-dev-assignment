b1f38a28fc9e13a5caa1d82daf3bab84a6961501
이민구 <goorm@goormui-MacBookPro.local>
2025-02-24 22:20:13 +0900

    Fix: 404에러 처리

--- src/main/java/org/covy/ktbbedevassignment/exception/NotFoundException.java
+++ src/main/java/org/covy/ktbbedevassignment/exception/NotFoundException.java
@@ -1,7 +1,6 @@
 package org.covy.ktbbedevassignment.exception;
 
 // 404에러 처리
-
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
